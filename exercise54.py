d1e9e9e78bcac2846d6aac26257ed39567d716c8
Troy Church <tchurch@berklee.edu>
2013-06-06 21:06:19 -0400

    triangle prompt method 1

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,19 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Troy
+
+def is_triangle(i1, i2, i3):
+	if (i1 + i2 + i3) / 3 == i1 and i2 and i3:
+		print "Yes"
+	else:
+		print "No"
+
+is_triangle(2, 2, 2)
+
+prompt = "Give 3 Angles: "
+angle1, angle2, angle3 = input(prompt)
+tuple(prompt)
+
+is_triangle(angle1, angle2, angle3)
+
+
+
